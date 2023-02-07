1e8d9a321d7d75da9f516bb28fbe6fb50d1dd35d
Michael <minimike100@gmail.com>
2023-02-07 13:37:01 +0000

    post page linked

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,29 @@
 import { useState } from 'react'
 import './App.css'
 import { Posts } from "./Component/Posts"
-import{BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { getPageOfPosts } from '../../src/services/postService'
 
 
 function App() {
+  // const [next,setNext]=useState<string|null>(null)
+  // const [previous,setPrevious]=useState<string|null>(null)
+
 
   return (
     <Router>
-    <div className="App">
-{   <Routes>
-        <Route path="/posts"element={<Posts/>}/>    
-        </Routes>
-     }
+      <div className="App">
+        
+        <Routes>
+              <Route path="/posts" 
+                     element={<Posts/>}>
 
-     <Posts/>
-     </div>
-     </Router>
+               </Route>
+               
+        </Routes>
+       
+      </div>
+    </Router>
   )
 }
 
--- frontend/src/Component/Posts.tsx
+++ frontend/src/Component/Posts.tsx
@@ -1,38 +1,56 @@
 import React, { useState, useEffect } from 'react';
-import {Page} from '../../../src/models/api/page'
-import {PostModel} from '../../../src/models/api/postModel'
-import {PostUserModel} from '../../../src/models/api/postModel'
-import {Moment} from 'moment'
+import { Page } from '../../../src/models/api/page'
+import { PostModel } from '../../../src/models/api/postModel'
+import { PostUserModel } from '../../../src/models/api/postModel'
+import { Moment } from 'moment'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 export function Posts() {
-    const [myData, setMyData] = useState<Page <PostModel> | null > (null);
+    const [myData, setMyData] = useState<Page<PostModel> | null>(null);
+    const [urlQuery, setUrlQuery] = useState<string | null > ("/posts");
     useEffect(() => {
-        fetch("http://localhost:3001/posts")
+        fetch(`http://localhost:3001${urlQuery}`)
             .then(response => response.json())
-            .then(response => setMyData(response));
-            console.log(myData);
-        
-    }, []);
+            .then(response => setMyData(response))
+
+    }, [urlQuery]);
     if (!myData) {
         return <div>Waiting for data!</div>
     }
-    
-    const posts = myData.results.map(post  => {
+    console.log(myData)
+    const posts = myData.results.map(post => {
         return (
             <div>
-                <img src={post.imageUrl}/>
-                <li>{post.id}</li>
-                <li>{post.message}</li>
-                <li>{post.imageUrl}</li>
+                <img src={post.imageUrl} alt={post.message} />
                 <li>{post.createdAt.toString()}</li>
-                <li>{post.postedBy.id}</li>
-                <li>{post.likedBy.length}</li>
-                <li>{post.dislikedBy.length}</li>
+                <li>{post.id}</li>
             </div>
         )
-    }) 
+    })
+
     return (
-       <div>{posts}</div>
+        <div>
+            <h1>Posts</h1>
+
+            <div>{posts}</div>
+            <div>{myData.previous ?
+                <Link onClick={() => setUrlQuery(myData.previous)} to={myData.previous}>Previous</Link> :
+                <></>}
+            
+                 {myData.next ?
+                <Link onClick={() => setUrlQuery(myData.next)} to={myData.next}>Next</Link> :
+                <></>}
+            </div>
+
+        </div>
     );
 }
 
+
+                // <li>{post.id}</li>
+                // <li>{post.message}</li>
+                // <li>{post.imageUrl}</li>
+                // <li>{post.createdAt.toString()}</li>
+                // <li>{post.postedBy.id}</li>
+                // <li>{post.likedBy.length}</li>
+                // <li>{post.dislikedBy.length}</li>
